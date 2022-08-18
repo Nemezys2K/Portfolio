8fbd419179a351d4ffc7893ec2138e65fd5df9e3
Nemezys2K <carvalho2604@outlook.com>
2022-08-18 10:28:22 -0300

    Saved1

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const router = require('./src/router');
 app.use('/pessoas', router);
+// app.use('/musicas', router);
+
 
 
 app.listen(3000, ()=>{console.log('Servidor rodando na porta 3000')});
--- src/router.js
+++ src/router.js
@@ -2,12 +2,28 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/clientes',(req,res)=>{
-    res.send("Ola usuário!");
-
+router.get('/',(req,res)=>{
+    // res.send("Ola usuário!");
+    const pessoas = require('./database/pessoas.json');
 })
 
 router.get('/:id', (req,res)=>{
     // res.send("Outro texto!");
+    const id = req.params.id;
+
+    const pessoas = require('./database/pessoas.json')
+    const pessoa = pessoas.find(p => p.id == id);
+
+    if (pessoa == undefined){
+        res.send("Pessoa inexistente");
+    }else{
+        res.send(pessoa);
+    }
 })
+
+// router.get('/musica',(req,res)=>{
+
+//     const musicas = require('./database/musicas.json')
+// })
+
 module.exports = router;
